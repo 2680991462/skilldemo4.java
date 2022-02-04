9f60187c970911115f06d3faa0d077df0c2260bf
hys12138- <yuh045@ucsd.edu>
2022-02-03 21:59:21 -0800

    starter and libs

--- skilldemo4.java
+++ skilldemo4.java
@@ -1,1 +1,5 @@
-public class skilldemo4{}
+public class skilldemo4{
+    public static int minus(int x, int y){
+        return x - y;
+    }
+}
---
+++ skilldemotest4.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skilldemotest4 {
+    @Test 
+    public void testminus(){
+        assertEquals(2, skilldemo4.minus(3, 2));
+    }
+}
